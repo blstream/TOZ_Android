31749e0fa2206bdf79bf1e88340ef41aa853539c
sswierczk <sebastian.swierczek@gmail.com>
2017-02-25 19:32:23 +0100

    Add checkstyle

--- app/src/main/java/com/intive/toz/MainActivity.java
+++ app/src/main/java/com/intive/toz/MainActivity.java
@@ -3,10 +3,14 @@ package com.intive.toz;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * FIXME Set proper name when implemented.
+ * Main app screen.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
