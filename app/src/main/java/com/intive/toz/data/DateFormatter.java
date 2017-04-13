6b5ad544ea8b4fc5e57c9d7cb55bc59cf05cd837
Michał Siedlecki <michal.siedlecki@intive.com>
2017-04-13 18:29:28 +0200

    Dodanie sortowania listy aktualności według daty utworzenia.

--- app/src/main/java/com/intive/toz/data/DateFormatter.java
+++ app/src/main/java/com/intive/toz/data/DateFormatter.java
@@ -3,6 +3,7 @@ package com.intive.toz.data;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  *  Class for formatting date from miliseconds to (dd/mm/yyyy).
@@ -11,6 +12,7 @@ import java.util.Calendar;
 public class DateFormatter {
     private DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
     private Calendar calendar = Calendar.getInstance();
+    private static final int MINUS_SIX_MONTHS = -6;
 
 
     /**
@@ -21,5 +23,17 @@ public class DateFormatter {
     public String convertToDate(final long dateInMilliseconds) {
         calendar.setTimeInMillis(dateInMilliseconds);
         return formatter.format(calendar.getTime());
+    }
+
+    /**
+     *
+     * @param createdDate news create date in milliseconds
+     * @return true if news create date is earlier than six months and false if not.
+     */
+    public boolean isLessThanSixMonth(final long createdDate) {
+        Calendar createNewsDate = new GregorianCalendar();
+        createNewsDate.setTimeInMillis(createdDate);
+        calendar.add(Calendar.MONTH, MINUS_SIX_MONTHS);
+        return createNewsDate.after(calendar);
     }
 }
