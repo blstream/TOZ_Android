2ee42c139d2e1433af17dd5289047a7ede08b298
Krzysztof Pawlak <kpawlak@wi.zut.edu.pl>
2017-04-13 19:33:20 +0200

    [FIX] conflict with develop

--- app/src/main/java/com/intive/toz/network/ApiClient.java
+++ app/src/main/java/com/intive/toz/network/ApiClient.java
@@ -1,5 +1,17 @@
 package com.intive.toz.network;
 
+import com.intive.toz.TozApplication;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Cache;
+import okhttp3.CacheControl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -8,7 +20,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 public final class ApiClient {
 
-    private static final String API_URL = "http://dev.patronage2017.intive-projects.com";
+//    private static final String API_URL = "http://dev.patronage2017.intive-projects.com";
     private static final String CACHE_CONTROL = "Cache-Control";
     private static final String CACHE_DIRECTORY = "cache";
     private static final int BUFFER_SIZE = 10485760;
