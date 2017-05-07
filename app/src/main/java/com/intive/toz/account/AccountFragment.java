59f1ab9b24d80bc483e10302cd4261954244998c
Krzysztof Palczewski <kpalcze@gmail.com>
2017-05-07 23:36:53 +0200

    [ADD] account tab

---
+++ app/src/main/java/com/intive/toz/account/AccountFragment.java
@@ -0,0 +1,52 @@
+package com.intive.toz.account;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.intive.toz.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
+
+/**
+ *  Fragment displaying options in tab 'account'.
+ */
+public class AccountFragment extends Fragment {
+
+    @BindView(R.id.change_password_btn)
+    Button changePassword;
+
+    private Unbinder unbinder;
+
+    public static AccountFragment newInstance() {
+        return new AccountFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_account, container, false);
+    }
+
+    @Override
+    public void onViewCreated(final View view, @Nullable final Bundle savedInstance) {
+        super.onViewCreated(view, savedInstance);
+        unbinder = ButterKnife.bind(this, view);
+    }
+
+    @OnClick(R.id.change_password_btn)
+    private void onChangePasswordClicked () {
+        ChangePasswordFragment changePasswordFragment = ChangePasswordFragment.newInstance();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.fragment_container, firstFragment).commit();
+    }
+}
