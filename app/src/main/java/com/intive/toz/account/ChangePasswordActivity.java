50a6f0e4e795bcbab339137f36bd0e2445ca3c57
Bartosz-Kozajda <bartosz.kozajda@gmail.com>
2017-05-28 21:46:43 +0200

    [FIX] wrong initials

--- app/src/main/java/com/intive/toz/account/ChangePasswordActivity.java
+++ app/src/main/java/com/intive/toz/account/ChangePasswordActivity.java
@@ -1,26 +1,20 @@
 package com.intive.toz.account;
 
-import android.content.Intent;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputEditText;
-import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Button;
 
 import com.hannesdorfmann.mosby3.mvp.MvpActivity;
-import com.intive.toz.MainActivity;
 import com.intive.toz.R;
 import com.intive.toz.common.view.calendar.SnackbarFactory;
-import com.intive.toz.login.LoginActivity;
-import com.intive.toz.login.Session;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 /**
  * Acitivity for changing password formular.
@@ -46,12 +40,6 @@ public class ChangePasswordActivity extends MvpActivity<ChangePasswordMVP.Change
     @BindView(R.id.input_repeat_new_password)
     TextInputEditText repeatedNewPassword;
 
-    @BindView(R.id.confirm_change_password)
-    Button changePassword;
-
-    @BindView(R.id.cancel_change_password)
-    Button cancelButton;
-
     private Snackbar snackbar;
 
     @NonNull
@@ -72,19 +60,10 @@ public class ChangePasswordActivity extends MvpActivity<ChangePasswordMVP.Change
     /**
      * Validate input passwords on button clicked.
      */
-    @OnClick(R.id.confirm_change_password)
-    public void onChangePasswordClicked() {
+    private void onChangePasswordClicked() {
         presenter.validatePassword(oldPassword.getText().toString(),
-                                    newPassword.getText().toString(),
-                                    repeatedNewPassword.getText().toString());
-    }
-
-    /**
-     * Go back to account tab when cancel button clicked.
-     */
-    @OnClick(R.id.cancel_change_password)
-    public void onCancelButtonClicked() {
-        finish();
+                newPassword.getText().toString(),
+                repeatedNewPassword.getText().toString());
     }
 
     @Override
@@ -168,7 +147,7 @@ public class ChangePasswordActivity extends MvpActivity<ChangePasswordMVP.Change
     @Override
     public boolean onCreateOptionsMenu(final Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
+        inflater.inflate(R.menu.add_pet_menu, menu);
         return true;
     }
 
@@ -177,31 +156,13 @@ public class ChangePasswordActivity extends MvpActivity<ChangePasswordMVP.Change
         switch (item.getItemId()) {
             case android.R.id.home:
                 this.finish();
-                return true;
-            case R.id.menu_log_in:
-                Intent logIn = new Intent(this, LoginActivity.class);
-                startActivity(logIn);
-                return true;
-            case R.id.menu_log_out:
-                Session.logOut();
-                Intent logOut = new Intent(this, MainActivity.class);
-                logOut.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(logOut);
-                return true;
+                break;
+            case R.id.action_save:
+                onChangePasswordClicked();
+                break;
             default:
-                return super.onOptionsItemSelected(item);
+                break;
         }
-    }
-
-    @Override
-    public boolean onPrepareOptionsMenu(final Menu menu) {
-        if (Session.isLogged()) {
-            menu.findItem(R.id.menu_log_in).setVisible(false);
-            menu.findItem(R.id.menu_log_out).setVisible(true);
-        } else {
-            menu.findItem(R.id.menu_log_in).setVisible(true);
-            menu.findItem(R.id.menu_log_out).setVisible(false);
-        }
-        return super.onPrepareOptionsMenu(menu);
+        return true;
     }
 }
