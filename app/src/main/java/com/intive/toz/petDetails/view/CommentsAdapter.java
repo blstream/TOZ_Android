2233904a4b3a9be6260e2c963c32f3be933be05b
Bartosz-Kozajda <bartosz.kozajda@gmail.com>
2017-06-05 14:15:31 +0200

    [ADD] edit comment view

--- app/src/main/java/com/intive/toz/petDetails/view/CommentsAdapter.java
+++ app/src/main/java/com/intive/toz/petDetails/view/CommentsAdapter.java
@@ -1,5 +1,6 @@
 package com.intive.toz.petDetails.view;
 
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.TextView;
 
 import com.intive.toz.R;
 import com.intive.toz.data.DateFormatter;
+import com.intive.toz.login.Session;
 import com.intive.toz.petDetails.model.Comment;
 
 import java.util.List;
@@ -52,6 +54,12 @@ public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.Commen
         holder.content.setText(comments.get(position).getContents());
         String date = "| " + formatter.convertToDate(comments.get(position).getCreated());
         holder.date.setText(date);
+
+        if (comments.get(position).getUserUuid().equals(Session.getUserId())) {
+            holder.editBtn.setVisibility(View.VISIBLE);
+        } else {
+            holder.editBtn.setVisibility(View.GONE);
+        }
     }
 
     @Override
@@ -83,6 +91,30 @@ public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.Commen
         TextView content;
 
         /**
+         * The Edit btn.
+         */
+        @BindView(R.id.edit_btn)
+        TextView editBtn;
+
+        /**
+         * The Add comment view.
+         */
+        @BindView(R.id.add_comment_view)
+        View addCommentView;
+
+        /**
+         * The Add comment et.
+         */
+        @BindView(R.id.add_comment_et)
+        TextInputEditText addCommentEt;
+
+        /**
+         * The Add comment btn.
+         */
+        @BindView(R.id.add_comment)
+        TextView addCommentBtn;
+
+        /**
          * Instantiates a new Comments view holder.
          *
          * @param itemView the item view
@@ -90,6 +122,15 @@ public class CommentsAdapter extends RecyclerView.Adapter<CommentsAdapter.Commen
         CommentsViewHolder(final View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
+            editBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    addCommentView.setVisibility(View.VISIBLE);
+                    addCommentEt.setText(content.getText());
+                }
+            });
+
+
         }
     }
 }
